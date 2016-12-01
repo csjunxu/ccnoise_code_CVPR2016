a468a4e4491f9cc6125977002d04e98a67d716fd
XuJun <nankaimathxujun@gmail.com>
2016-12-01 17:27:29 +0800

    add

---
+++ test.m
@@ -0,0 +1,25 @@
+clear;
+TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_NoisyImage\';
+TT_fpath = fullfile(TT_Original_image_dir, '*.png');
+% TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_ccnoise_denoised_part\';
+% TT_fpath = fullfile(TT_Original_image_dir, '*real.png');
+TT_im_dir  = dir(TT_fpath);
+im_num = length(TT_im_dir);
+load ccnoise-master/C.mat
+for i = 1:im_num
+    IMin = im2double(imread(fullfile(TT_Original_image_dir, TT_im_dir(i).name)));
+    S = regexp(TT_im_dir(i).name, '\.', 'split');
+    IMname = S{1};
+    fprintf('%s : \n',IMname);
+    Options.kernelratio=3;
+    Options.windowratio=21;
+    Options.verbose=true;
+    Options.filterstrength=0.1;
+    Options.nThreads=8;
+    Options.blocksize=128;
+    Options.icov=Covs{i,1};
+    IMout=NLMF(IMin,Options);
+    figure,
+    subplot(1,2,1),imshow(IMin); title('Noisy image')
+    subplot(1,2,2),imshow(IMout); title('NL-means image');
+end
