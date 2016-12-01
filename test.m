2b2570dda4e911498a574a52bcfa76303f89763c
XuJun <nankaimathxujun@gmail.com>
2016-12-01 17:34:55 +0800

    ad

--- test.m
+++ test.m
@@ -5,21 +5,22 @@ TT_fpath = fullfile(TT_Original_image_dir, '*.png');
 % TT_fpath = fullfile(TT_Original_image_dir, '*real.png');
 TT_im_dir  = dir(TT_fpath);
 im_num = length(TT_im_dir);
-load ccnoise-master/C.mat
+load ccnoise-master/invC.mat
 for i = 1:im_num
     IMin = im2double(imread(fullfile(TT_Original_image_dir, TT_im_dir(i).name)));
     S = regexp(TT_im_dir(i).name, '\.', 'split');
     IMname = S{1};
     fprintf('%s : \n',IMname);
-    Options.kernelratio=3;
-    Options.windowratio=21;
-    Options.verbose=true;
-    Options.filterstrength=0.1;
-    Options.nThreads=8;
-    Options.blocksize=128;
-    Options.icov=Covs{i,1};
+    Options.kernelratio     = 3;
+    Options.windowratio  = 21;
+    Options.verbose         = true;
+    Options.filterstrength = 0.1;
+    Options.nThreads       = 8;
+    Options.blocksize       = 128;
+    Options.icov               = iCovs{i,1};
     IMout=NLMF(IMin,Options);
-    figure,
-    subplot(1,2,1),imshow(IMin); title('Noisy image')
-    subplot(1,2,2),imshow(IMout); title('NL-means image');
+    imwrite(IMout, ['CCNoise_' IMname '.png']);       
+%     figure,
+%     subplot(1,2,1),imshow(IMin); title('Noisy image')
+%     subplot(1,2,2),imshow(IMout); title('NL-means image');
 end
